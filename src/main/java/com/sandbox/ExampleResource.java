c9a75a6abcaf4a0295d30ac20ee027516c8012d3
fred <fredericond@hotmail.com>
2020-04-13 18:22:24 -0300

    ajustando caminho de endpoint hello

--- src/main/java/com/sandbox/ExampleResource.java
+++ src/main/java/com/sandbox/ExampleResource.java
@@ -5,12 +5,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/as")
+@Path("/hello")
 public class ExampleResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "kiko";
+        return "hello";
     }
 }
